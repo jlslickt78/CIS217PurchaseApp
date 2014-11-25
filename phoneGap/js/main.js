317337351c916a0ddd4f9a05b710d48c64fc3c91
Jeff Tartt <jlslickt78@yahoo.com>
2014-11-24 20:17:46 -0800

    added geolocation

--- phoneGap/js/main.js
+++ phoneGap/js/main.js
@@ -153,14 +153,34 @@ $(document).ready(function () {
         $("#map").html(map);
     });
 
-    //contact picker
-   $(".openContacts").on("click", function(){
-      navigator.contacts.pickContact(function(contact){
-          console.log('The following contact has been selected:' + JSON.stringify(contact));
-      }, function(error){
-          console.log("Error: " + error);
-      });
-   });
+    //gps
+    $(".openGPS").on("click", function(){
+        // onSuccess Callback
+// This method accepts a Position object, which contains the
+// current GPS coordinates
+//
+        var onSuccess = function(position) {
+            alert('Latitude: '        + position.coords.latitude          + '\n' +
+                'Longitude: '         + position.coords.longitude         + '\n' +
+                'Altitude: '          + position.coords.altitude          + '\n' +
+                'Accuracy: '          + position.coords.accuracy          + '\n' +
+                'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+                'Heading: '           + position.coords.heading           + '\n' +
+                'Speed: '             + position.coords.speed             + '\n' +
+                'Timestamp: '         + position.timestamp                + '\n');
+        };
+
+// onError Callback receives a PositionError object
+//
+        function onError(error) {
+            alert('code: '    + error.code    + '\n' +
+                'message: ' + error.message + '\n');
+        }
+
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    });
+
+
 
 });
 
